8a3e35e4e9f704fd746728dca2d787cc9283aa36
Loïc <lsciampagna@agilepartner.net>
2022-04-26 12:19:24 +0200

    revamp header

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const burgerButton = document.querySelector('#burger');
-    const header = document.querySelector('.ap-header');
-    
+	// "document" est notre document HTML, on va aller chercher la div #burger dans notre document grâce à querySelector et on le place dans une constante qu'on appelle burgerButton
+	const burgerButton = document.querySelector('#burger');
 
-    burgerButton.addEventListener('click', function() {
-        header.classList.toggle('ap-header--show-nav');
-    });
+	// On écoute tous les clics qui peuvent se produire sur le bouton burgerButton, dès qu'un clic est détecté on appelle une fonction qui va toggle la class show-nav sur le body
+	burgerButton.addEventListener('click', function() {
+		document.body.classList.toggle('show-nav');
+	});
 })
 
