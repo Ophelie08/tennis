6c9971719d1607b50e7a9c488d27e2aa89b9ce68
Loïc <lsciampagna@agilepartner.net>
2022-05-02 13:14:21 +0200

    fix lint

---
+++ server.js
@@ -0,0 +1,22 @@
+const livereload = require("livereload");
+const express = require("express");
+const path = require("path");
+const connectLivereload = require("connect-livereload");
+
+// open livereload high port and start to watch public directory for changes
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+// ping browser on Express boot, once browser has reconnected and handshaken
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+const app = express();
+
+app.listen(3000);
+
+// monkey patch every served HTML so they know of changes
+app.use(connectLivereload());
