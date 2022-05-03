6639df8c56bb6f39610c351801bff9a244aa7943
Loïc <lsciampagna@agilepartner.net>
2022-05-04 00:41:45 +0200

    add new ap-player-card component + clean code

--- js/script.js
+++ js/script.js
@@ -10,8 +10,57 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	}
 
-	if ("import" in document.createElement("link")) {
-		console.log('ici')
+
+	// De la même manière que pour le burger menu on va aller chercher dans notre document HTML l'élément qui se nomme #playersArea et s'il existe (if)
+	// alors on le stocke dans une constante qui se nomme playersArea
+	if(document.querySelector('#playersArea')) {
+		const playersArea = document.querySelector('#playersArea');
+
+		// Création d'un tableau [] d'objets {}.
+		// Un objet {} est un ensemble de propriétés
+		// Une propriété est l'association d'une clé et d'une valeur (image = clé et 'assets/images/balle-tennis.jpg' = valeur)
+		const players = [
+			{
+				image: 'assets/images/balle-tennis.jpg',
+				name: 'Reggianini Sarah',
+				nationality: 'FR',
+				flag: 'assets/images/flags/fr.jpg',
+				ranking: '# 15/1',
+				age: '16 ans',
+				club: 'T.C GUENANGE'
+			},
+			{
+				image: 'assets/images/balle-tennis.jpg',
+				name: 'Reggianini Sarah',
+				nationality: 'FR',
+				flag: 'assets/images/flags/fr.jpg',
+				ranking: '# 15/1',
+				age: '16 ans',
+				club: 'T.C GUENANGE'
+			}
+		];
+
+		// On créé un tableau [] playersElements vide. Plus tard il sera peuplé avec le code HTML de ap-player-card
+		let playersElements = [];
+
+		// Pour chaque objet player qui se trouve dans mon tableau players alors je pousse à l'aide de la méthode .push() le code html de ap-player-card
+		players.forEach(player => {
+			playersElements.push(`<div class="ap-grid__column ap-grid__column--md--3 margin-bottom--32">
+				<ap-player-card
+					image="${player.image}"
+					name="${player.name}"
+					nationality="${player.nationality}"
+					flag="${player.flag}"
+					ranking="${player.ranking}"
+					age="${player.age}"
+					club="${player.club}"
+				></ap-player-card>
+			</div>`
+			)
+		})
+
+		// à l'intérieur de mon élément #playersArea je vais placer mon tableau de players
+		playersArea.innerHTML = playersElements.join('');
 	}
 })
 
