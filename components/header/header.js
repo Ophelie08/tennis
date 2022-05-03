dd4666fb1f76dc7bc6b740ee0ba3d61ab6394d5c
Loïc <lsciampagna@agilepartner.net>
2022-05-03 16:09:31 +0200

    add server + live reload

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,66 @@
 class Header extends HTMLElement {
+
 	constructor() {
-    // Always call super first in constructor
-    super();
+		super();
+
+		this._selected = "";
 	}
 
 	connectedCallback() {
-	 this.innerHTML = `test`
+		this._selected = this.getAttribute('selected');
+
+	 	this.innerHTML = `<header class="ap-header">
+			<div class="ap-header__bar">
+				<img class="ap-header__logo" src="assets/images/logo.svg" alt="logo waop" />
+				<div class="ap-burger" id="burger">
+					<div class="ap-burger__line"></div>
+					<div class="ap-burger__line"></div>
+					<div class="ap-burger__line"></div>
+				</div>
+			</div>
+
+			<nav class="ap-header__nav">
+				<ul class="ap-header__nav__ul">
+					<li class="ap-header__nav__li ${this._selected === 'home' && 'ap-header__nav__li--selected'}">
+						<a class="ap-header__nav__link ap-header__nav__link--selected" href="/">
+							<i class="fa-solid fa-home fa-lg"></i>
+							<span>Accueil</span>
+						</a>
+					</li>
+					<li class="ap-header__nav__li ${this._selected === 'table' && 'ap-header__nav__li--selected'}">
+						<a class="ap-header__nav__link" href="table">
+							<i class="fa-solid fa-table-columns fa-lg"></i>
+							<span>Tableau</span>
+						</a>
+					</li>
+					<li class="ap-header__nav__li ${this._selected === 'history' && 'ap-header__nav__li--selected'}">
+						<a class="ap-header__nav__link" href="history">
+							<i class="fa-solid fa-book fa-lg"></i>
+							<span>Histoire</span>
+						</a>
+					</li>
+					<li class="ap-header__nav__li ${this._selected === 'players' && 'ap-header__nav__li--selected'}">
+						<a class="ap-header__nav__link" href="players">
+							<i class="fa-solid fa-users fa-lg"></i>
+							<span>Joueuses</span>
+						</a>
+					</li>
+					<li class="ap-header__nav__li ${this._selected === 'program' && 'ap-header__nav__li--selected'}">
+						<a class="ap-header__nav__link" href="program">
+							<i class="fa-solid fa-list fa-lg"></i>
+							<span>Programme</span>
+						</a>
+					</li>
+					<li class="ap-header__nav__li">
+						<a class="ap-header__nav__link" target="_blank" href="https://www.youtube.com/channel/UC5cSlOClwHK6FaAH13OU-tw">
+							<i class="fa-brands fa-youtube fa-lg"></i>
+							<span>Matchs en live</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	`
 	}
 }
 
